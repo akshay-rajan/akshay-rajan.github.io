298bb16fdf417a0f0a9f68a6b22cfd97c6a5f884
Akshay Rajan <akshayrajan345@gmail.com>
2024-03-03 22:39:38 +0530

    Projects

--- portfolio.js
+++ portfolio.js
@@ -3,22 +3,27 @@ function displayCard() {
         '1': {
             'name': 'DeviceDash',
             'details': 'Smartphone Recommendation Website',
+            'github': 'devicedash'
         },
         '2': {
             'name': 'Timer For Chrome',
             'details': 'A Timer Chrome Extension',
+            'github': 'timer'
         },
         '3': {
             'name': 'Auctions',
             'details': 'Auction Website',
+            'github': 'cs50w/blob/main/commerce'
         },
         '4': {
             'name': 'Ripple',
             'details': 'A Twitter-like Social Media App',
+            'github': 'cs50w/tree/main/network'
         },
         '5': {
-            'name': 'Numerical Analysis',
+            'name': 'Numerical Methods',
             'details': 'Solving Mathematical Problems using Numerical Analysis in Python',
+            'github': 'numericalmethods'
         },
     };
     let cardsDiv = document.querySelector('.project-cards');
@@ -30,5 +35,8 @@ function displayCard() {
                                 <p class='card-para'>${project['details']}</p>
                             </div>`;
         cardsDiv.appendChild(cardDiv);
+        cardDiv.addEventListener('click', () => {
+            window.location.href = `https://www.github.com/akshay-rajan/${project['github']}`;
+        });
     });
 }
