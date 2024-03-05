4a3b27a63ecc612b8a5d1c60713ad58704f1063b
Akshay Rajan <akshayrajan345@gmail.com>
2024-03-05 15:13:42 +0530

    Portfolio Cards Display

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,12 @@
+let cardsDiv = document.querySelector('.project-cards');
+
 function displayCard() {
     let projects = {
         '1': {
             'name': 'DeviceDash',
             'details': 'Smartphone Recommendation Website',
-            'github': 'devicedash'
+            'github': 'devicedash',
+            'description': 'DeviceDash is a smartphone recommendation website that helps users find the best smartphone for their needs. It uses a machine learning model to predict the best smartphone for the user based on their preferences. The website is built using Django and the machine learning model is built using Python.'
         },
         '2': {
             'name': 'Timer For Chrome',
@@ -26,7 +29,6 @@ function displayCard() {
             'github': 'numericalmethods'
         },
     };
-    let cardsDiv = document.querySelector('.project-cards');
     Object.values(projects).forEach(project => {
         let cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
@@ -36,7 +38,36 @@ function displayCard() {
                             </div>`;
         cardsDiv.appendChild(cardDiv);
         cardDiv.addEventListener('click', () => {
-            window.location.href = `https://www.github.com/akshay-rajan/${project['github']}`;
+            displayProject(project, cardDiv);
+            // window.location.href = `https://www.github.com/akshay-rajan/${project['github']}`;
         });
     });
+}
+
+function displayProject(project, cardDiv) {
+    // * If the project is displayed, hide it
+    if (cardDiv.classList.contains('card-expanded')) {
+        cardDiv.innerHTML = `<div class='card-content'>
+                                <p class='card-title'>${project['name']}</p>
+                                <p class='card-para'>${project['details']}</p>
+                            </div>`;
+        cardDiv.classList.remove('card-expanded');
+        return;
+    }
+    // * Hide any other project that is displayed
+    Array.from(cardsDiv.children).forEach(card => {
+        if (card.classList.contains('card-expanded')) {
+            card.querySelector('.card-desc').remove();
+            card.querySelector('.github-link').remove();            
+            card.classList.remove('card-expanded');
+        }
+    });
+    // * Display the project
+    cardDiv.classList.add('card-expanded');
+    cardDiv.innerHTML = `<div class='card-content'>
+                            <p class='card-title'>${project['name']}</p>
+                            <p class='card-para'>${project['details']}</p>
+                            <p class='card-desc'>${project['description']}</p>
+                            <button class='github-link' onclick="window.location.href='https://www.github.com/akshay-rajan/${project['github']}'"><icon class="fab fa-github"></icon> View on GitHub</button>
+                        </div>`;
 }
