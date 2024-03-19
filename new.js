4e6ad38129cd1f034a720f171eb8040dfa6ba034
Akshay Rajan <akshayrajan345@gmail.com>
2024-03-19 11:29:09 +0530

    Add Skills animation

--- new.js
+++ new.js
@@ -1,1 +1,31 @@
-// ToDo
+function animateSkills() {
+    let skillsDiv = document.querySelector('.skills');
+    let skills = ['Django', 'JavaScript', 'Flask', 'Python', 'HTML', 'CSS', 'Bootstrap', 'SQL', 'C', 'Java', 'Git', 'GitHub', 'Linux', 'BeautifuSoup', 'PHP', 'MySQL'];
+    let index = 0;
+
+    function addSkill() {
+        let currentSkill = document.createElement('div');
+        currentSkill.classList.add('skill');
+        currentSkill.innerText = skills[index % skills.length];
+        currentSkill.style.animation = `fadeInOut 2s ease-in-out forwards`;
+        skillsDiv.appendChild(currentSkill);
+
+        currentSkill.addEventListener('animationend', () => {
+            setTimeout(() => {
+                skillsDiv.removeChild(currentSkill);
+                index++;
+                addSkill();
+            }, 1000);
+        });
+    }
+    addSkill();
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Skills
+    animateSkills();
+
+});
