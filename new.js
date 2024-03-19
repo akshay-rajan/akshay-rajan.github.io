70516953a7bb566d52d68e3a779dfc62d3d063c2
Akshay Rajan <akshayrajan345@gmail.com>
2024-03-19 20:01:36 +0530

    Add Links

--- new.js
+++ new.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-
+// * Animate Skill display below name
 function animateSkills() {
     let skillsDiv = document.querySelector('.skills');
     let skills = ['Django', 'JavaScript', 'Flask', 'Python', 'HTML', 'CSS', 'Bootstrap', 'SQL', 'C', 'Java', 'Git', 'GitHub', 'Linux', 'BeautifuSoup', 'PHP', 'MySQL'];
@@ -29,8 +29,39 @@ function animateSkills() {
     addSkill();
 };
 
-document.querySelector('.dropdown-btn').addEventListener('click', function() {
-    document.querySelector('.dropdown-menu').classList.add('show');
-});
+// * Add personal links corresponding to the device
+function addLinks() {
+    let linksHTML = `<div class="links">
+        <div class="link-1">
+            <a href="#">
+            <img src="static/linkedin-icon.png" alt="">
+            </a>
+        </div>
+        <div class="link-2">
+            <a href="#">
+            <img src="static/github-icon.png" alt="">
+            </a>
+        </div>
+        <div class="link-3">
+            <a href="#">
+            <img src="static/email-icon.png" alt="">
+            </a>
+        </div>
+        <div class="link-4">
+            <a href="#">
+            <img src="static/phone-icon.png" alt="">
+            </a>
+        </div>
+    </div>`;
 
+    // For smartphones
+    if (window.innerWidth <= 768)
+        document.querySelector('.second').innerHTML = linksHTML;
+    else
+    // For large Screens
+        document.querySelector('.fourth').innerHTML = linksHTML;    
+}
+
+window.onload = addLinks;
+window.onresize = addLinks;
 
