b35f5576a89907f1acd32f1d06e0f75bf2c230fc
Akshay Rajan <akshayrajan345@gmail.com>
2024-04-13 19:08:18 +0530

    Form script added

--- index.js
+++ index.js
@@ -421,3 +421,14 @@ function hideCards(current=null) {
     });
 }
 
+
+// * Contact Form - Connect to Sheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzks4jFjaFkVQEuvAHol0hJmw86jni2yFubSw-wmSZtgIViK-Kb7VwTFXWA0bfBXng/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
