ce8c7fee80ec8db53fd83f29cde0bdc9799dd561
Akshay R <akshayrajan345@gmail.com>
2025-08-15 21:48:10 +0530

    Add new build time script

--- scripts/add-build-time.js
+++ scripts/add-build-time.js
@@ -1,7 +1,11 @@
-// scripts/fetch-meta.js
 import fs from "fs";
 
-const now = new Date().toISOString();
+const now = new Date();
+const day = String(now.getDate()).padStart(2, '0');
+const month = String(now.getMonth() + 1).padStart(2, '0');
+const year = now.getFullYear();
+const formattedDate = `${day}-${month}-${year}`;
+
 const filePath = "src/config.json";
 
 let existing = {};
@@ -9,12 +13,10 @@ if (fs.existsSync(filePath)) {
   const raw = fs.readFileSync(filePath, "utf-8");
   try {
     existing = JSON.parse(raw);
+    existing.builtAt = formattedDate;
+    fs.writeFileSync(filePath, JSON.stringify(existing, null, 2));
+    console.log("Added build time to config:", formattedDate);
   } catch {
-    console.error("Invalid JSON in config file. Overwriting.");
+    console.error("Invalid JSON in config file. Terminating Build Time addition.");
   }
 }
-
-existing.builtAt = now;
-
-fs.writeFileSync(filePath, JSON.stringify(existing, null, 2));
-console.log("Added build time to config:", now);
