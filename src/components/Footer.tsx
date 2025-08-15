cd2a955f8debc280a8af0382523e85276869f9ce
Akshay R <akshayrajan345@gmail.com>
2025-08-15 21:49:26 +0530

    Fix date bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <p>Designed in <em>Figma</em> and developed using <em>React</em> and <em>Sass</em>. Deployed to <em>Github Pages</em>. </p>
-      <p className="last-updated">Last Updated: {new Date(BUILT_AT).toLocaleDateString()}</p>
+      <p className="last-updated">Last Updated: {BUILT_AT}</p>
     </footer>
   );
 }
