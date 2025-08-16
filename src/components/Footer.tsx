ee22bf333d41ae602918baf0f2fd7dabd388ff08
Akshay R <akshayrajan345@gmail.com>
2025-08-16 11:06:38 +0530

    Fix additional skill styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { BUILT_AT } from "../config";
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <p>Designed in <em>Figma</em> and developed using <em>React</em> and <em>Sass</em>. Deployed to <em>Github Pages</em>. </p>
+      <p>Designed in <em>Figma</em> and built in <em>React</em>. Styled with <em>Sass</em>. Deployed to <em>Github Pages</em>. </p>
       <p className="last-updated">Last Updated: {BUILT_AT}</p>
     </footer>
   );
