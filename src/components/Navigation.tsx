1ff573f8922297a2d72fc221cb8db5473af36661
Akshay R <akshayrajan345@gmail.com>
2025-08-17 00:46:49 +0530

    Add responsive navigation menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { useActiveSectionContext } from "../context/ActiveSectionContext";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaChevronRight } from "react-icons/fa";
+
 
 const Navigation: React.FC = () => {
   const activeSection = useActiveSectionContext();
+  const [isMobile, setIsMobile] = React.useState(false);
+  const [isVisible, setIsVisible] = React.useState(true);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 798);
+    };
+
+    handleResize(); // Set initial state
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function toggleMobileMenu() {
+    setIsVisible(!isVisible);
+    const navElement = document.getElementById('nav');
+    navElement?.classList.toggle('visible', !isVisible);
+  }
 
   const navItems = [
     { href: "#experience", label: "Experience", id: "experience" },
@@ -13,15 +37,34 @@ const Navigation: React.FC = () => {
   ];
 
   return (
-    <nav className="navigation">
-      {navItems.map(({ href, label, id }) => (
-        <a key={id} href={href}>
-          <div className={activeSection === id ? 'active' : ''}>
-            {label}
-          </div>
-        </a>
-      ))}
-    </nav>
+    <>
+      {isMobile && (
+        <button
+          className="toggle-button" 
+          onClick={toggleMobileMenu}
+        >
+          {isVisible ? 
+            <RxHamburgerMenu className="hamburger-icon" /> :
+            <FaChevronRight className="chevron-icon" />
+          }
+        </button>
+      )}
+      <nav className="navigation visible" id="nav">
+        <div>
+          {navItems.map(({ href, label, id }) => (
+            <a 
+              key={id} 
+              href={href}
+              onClick={toggleMobileMenu}
+            >
+              <div className={activeSection === id ? 'active' : ''}>
+                {label}
+              </div>
+            </a>
+          ))}
+        </div>
+      </nav>
+    </>
   );
 }
 
